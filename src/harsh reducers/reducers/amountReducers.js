e3deb0780a8b992f3769cc629d8747d0d621142f
harsh <harshkhosla9945@gmail.com>
2022-07-29 10:49:57 +0530

    remove debugger

--- src/harsh reducers/reducers/amountReducers.js
+++ src/harsh reducers/reducers/amountReducers.js
@@ -7,18 +7,17 @@ const initialState={
 const reducer =(state=initialState,action)=>{
 //    debugger
     if(action.type==="authtoken"){
-        // debugger;
+        
         return {...state ,authtoken:action.payload}
         
     }else if (action.type==="logOut"){
-        // debugger
+    
         return{authtoken:null}
     }else if (action.type==="alltheData"){
-        // debugger;
         return{...state,notesData:action.payload}
         
     }else if (action.type==="searchData"){
-        // debugger;
+    
         return{...state,searchData:action.payload}        
     }
     return state
